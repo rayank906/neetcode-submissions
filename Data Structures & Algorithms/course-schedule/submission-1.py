b421a7bf21c85c32dcb74f3610cd34de1929d705
Rayan Kamdem <rayankdm@umich.edu>
2026-08-11 22:31:20 -0400

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.py
@@ -0,0 +1,39 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """
+            - build adj list of nodes
+            - perform dfs using visit set. If a node was seen,
+            cycle must exist so impossible return false
+
+            TC: O(V+E)
+            SC: O(V+E)
+        """
+        graph = {}
+        for course, pre in prerequisites:
+            if course not in graph:
+                graph[course] = []
+            if pre not in graph:
+                graph[pre] = []
+            graph[course].append(pre)
+        
+        visit = set()
+        def dfs(course):
+            if not graph[course]:
+                return True
+            visit.add(course)
+            for pre in graph[course]:
+                if pre in visit:
+                    return False
+                if not dfs(pre):
+                    return False
+            visit.remove(course)
+            graph[course] = []
+            return True
+        
+        for course in graph:
+            if not dfs(course):
+                return False
+        return True
+
+
+        
