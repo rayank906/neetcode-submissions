58850a83255cb0e73a6b3759568596df9195f8d7
Rayan Kamdem <rayankdm@umich.edu>
2026-08-29 13:31:49 -0400

    Add: house-robber - submission-3

---
+++ Data Structures & Algorithms/house-robber/submission-3.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        """
+            - if 1 elem, return element
+            - init dp arr of 2
+            - at any i:
+                - rob i and add dp[1]
+                - take dp[0]
+            - return max(dp)
+        """
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        dp = [nums[n - 2], nums[n - 1]]
+        for i in range(n-3, -1, -1):
+            temp = max(nums[i] + dp[1], dp[0])
+            dp[1] = max(dp)
+            dp[0] = temp
+        return max(dp)
+        
